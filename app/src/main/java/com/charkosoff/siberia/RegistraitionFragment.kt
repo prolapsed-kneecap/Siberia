d13dc6577480d383d7ff8ab31fa8a2d1d8274974
[user5] <[user5@mail.ru]>
2021-04-17 16:21:46 +0700

    viewPager and tab_layout

--- app/src/main/java/com/charkosoff/siberia/RegistraitionFragment.kt
+++ app/src/main/java/com/charkosoff/siberia/RegistraitionFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 
 class RegistraitionFragment: Fragment() {
@@ -13,7 +17,20 @@ class RegistraitionFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = LayoutInflater.from(this.requireContext()).inflate(R.layout.fragment_registraition, container)
+        val nameEditText: EditText = view.findViewById(R.id.editTextName)
+        val groupEditText: EditText = view.findViewById(R.id.editTextGroup)
+        val inButton: Button = view.findViewById(R.id.button)
 
+        var user = User("Имя", "Группа")
+        nameEditText.doOnTextChanged { text, start, before, count ->
+            user.name = text.toString()
+        }
+        groupEditText.doOnTextChanged { text, start, before, count ->
+            user.group = text.toString()
+        }
+        inButton.setOnClickListener {
+            Toast.makeText(context, user.name+" "+user.group, Toast.LENGTH_LONG).show()
+        }
         return view
     }
 }

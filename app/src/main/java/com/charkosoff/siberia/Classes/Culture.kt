4fd7ca6219e2061f4685f48099272b72537c87ac
EV-55 <vasiliev_egor_55@mail.ru>
2021-04-19 13:25:00 +0700

    начал делать проверку культуры и создал класс культуры

---
+++ app/src/main/java/com/charkosoff/siberia/Classes/Culture.kt
@@ -0,0 +1,16 @@
+package com.charkosoff.siberia.Classes
+
+class Culture(val name:String) {
+}
+
+
+
+object Plants {
+    var cultures: MutableList<Culture> = mutableListOf(
+        Culture("Овёс"),
+                Culture("Пшеница"),
+                Culture("Ячмень"),
+                Culture("Горох"),
+                Culture("Фасоль")
+)
+}

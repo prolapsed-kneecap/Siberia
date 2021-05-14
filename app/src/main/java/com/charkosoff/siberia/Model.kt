b82ad938d55646b40c69d45859960adb7903934e
Psopis <konevvik05@mail.ru>
2021-05-15 04:58:37 +0700

    dekadi

--- app/src/main/java/com/charkosoff/siberia/Model.kt
+++ app/src/main/java/com/charkosoff/siberia/Model.kt
@@ -7,10 +7,14 @@ import android.widget.Toast
 class Model():TimeInterface{
     private var time:Long = 0
     private val timeTable = mapOf<LongRange, String>(
-        0..5000L to "Март",
-        5000..10000L to "Апрель",
-        10000..15000L to "Май",
-        15000..20000L to "Июнь"
+        0..2500L to "МартⅠ",
+        2500..5000L to "МартмъⅡ",
+        5000..7500L to "АпрельⅠ",
+        7500..10000L to "АпрельⅡ",
+        10000..12500L to "МайⅠ",
+        12500..15000L to "МайⅡ",
+        15000..17500L to "ИюньⅠ",
+        17500..20000L to "ИюньⅡ",
     )
     var countDownTimer = object:CountDownTimer(20000, 1000) {
         override fun onTick(millisUntilFinished: Long) {

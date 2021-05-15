d8133aa5a2138964b611d3fed83ca7cc95b69d00
Ctulhu <cry01@ya.ru>
2021-05-15 11:13:37 +0700

    start setup tapTarget

---
+++ app/src/main/java/com/charkosoff/siberia/utils/StatusUtils.kt
@@ -0,0 +1,31 @@
+package com.charkosoff.siberia.utils
+
+import android.content.Context
+
+object StatusUtils {
+    // 1
+    fun storeStatus(context: Context, show: Boolean) {
+        val preferences = context.getSharedPreferences("showStatus", Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor?.putBoolean("show", show)
+        editor?.apply()
+    }
+
+    // 2
+    fun getStatus(context: Context): Boolean {
+        val preferences = context.getSharedPreferences("showStatus", Context.MODE_PRIVATE)
+        return preferences.getBoolean("show", true)
+    }
+
+    fun storeStatusMain(context: Context, show: Boolean) {
+        val preferences = context.getSharedPreferences("showStatus", Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putBoolean("show", show)
+        editor.apply()
+    }
+
+    fun getStatusMain(context: Context): Boolean {
+        val preferences = context.getSharedPreferences("showStatus", Context.MODE_PRIVATE)
+        return preferences.getBoolean("show", true)
+    }
+}
